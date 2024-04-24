51a77bdeb5d4f782b43a739a5b00591287608a0a
sadorlin <sadorlin@student.42.fr>
2024-04-24 05:47:24 +0200

    correction blocage fetch ip a tester

--- src/components/Geoloc.ts
+++ src/components/Geoloc.ts
@@ -1,5 +1,7 @@
-
-export const findIp = async (): Promise<{latitude: number, longitude: number}> => {
+export const findIp = async (): Promise<{
+  latitude: number;
+  longitude: number;
+}> => {
   const response = await fetch("http://ip-api.com/json");
   if (!response.ok) {
     throw new Error("Failed to fetch IP address");
@@ -11,14 +13,36 @@ export const findIp = async (): Promise<{latitude: number, longitude: number}> =
   return { latitude: data.lat, longitude: data.lon };
 };
 
+export const getNavigatorPosition = (): Promise<GeolocationPosition> => {
+  return new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        position.coords.latitude;
+        position.coords.longitude;
+        resolve(position);
+      },
+      (error) => {
+        reject(error);
+      }
+    );
+  });
+};
+
 export default async function getPosition(): Promise<{
   latitude: number;
   longitude: number;
 }> {
+  try {
+    return await findIp();
+  } catch (error) {
     try {
-      return findIp();
+      const coords = await getNavigatorPosition();
+      return {
+        latitude: coords.coords.latitude,
+        longitude: coords.coords.longitude,
+      };
     } catch (error) {
       return { latitude: 0, longitude: 0 };
     }
+  }
 }
-
